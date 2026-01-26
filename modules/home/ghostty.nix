aa829919d86d93761987e363cc6117365ed776d5
playfairs <root@playfairs.cc>
2026-01-26 16:17:01 -0600

    changed ghostty opacity

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -13,7 +13,7 @@
 
     settings = {
       theme = "Rose Pine";
-      background-opacity = 0.80;
+      background-opacity = 0.70;
       macos-titlebar-style = "hidden";
       quit-after-last-window-closed = true;
       window-save-state = "never";
