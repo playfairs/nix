a33bb9be0cbc3a3da97bf53388ed7225e0f82e68
= <=>
2026-03-17 23:14:36 -0500

    meoq

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -46,7 +46,7 @@ lib.optionalAttrs linux {
       ];
 
       exec = [
-        "swww img ${flakeConfig.user.wallpaper or ../../wallpapers/nixos-wallpaper-catppuccin-mocha.png}"
+        "swww img ${flakeConfig.user.wallpaper or ../../wallpapers/image.png}"
       ];
 
       # General settings
