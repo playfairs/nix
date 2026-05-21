2b0fbd9652920a79256c5084825e32ad3142214f
playfairs <root@playfairs.cc>
2026-05-20 22:39:03 -0500

    update wallappers and ghostty opacity

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -101,6 +101,6 @@ lib.optionalAttrs (!linux) {
 
   programs.setWallpaper = {
     enable = true;
-    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/saturn2.png;
+    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/freebsd.png;
   };
 }
