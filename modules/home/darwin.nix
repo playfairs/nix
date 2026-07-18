ce5b8f271c574cb3917717df6dcaeeda84d1b3df
playfairs <root@playfairs.cc>
2026-07-18 10:32:21 -0500

    update wallpaper

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -101,6 +101,6 @@ lib.optionalAttrs (!linux) {
 
   programs.setWallpaper = {
     enable = true;
-    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/ani.jpg;
+    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/kamenrider.jpg;
   };
 }
