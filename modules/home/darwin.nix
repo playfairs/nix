2738269e8b5034e349ee57716febecfe6ce9612d
playfairs <root@playfairs.cc>
2026-07-20 01:30:15 -0500

    darwin: update wallpaper

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -101,6 +101,6 @@ lib.optionalAttrs (!linux) {
 
   programs.setWallpaper = {
     enable = true;
-    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/misc/earth.jpg;
+    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/nix/nixos-wallpaper-catppuccin-mocha.png;
   };
 }
