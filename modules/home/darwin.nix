d8d6475acc4bd5789bf2c152b9a7955c003cb6b6
playfairs <root@playfairs.cc>
2026-07-26 10:16:56 -0500

    nix (darwin): update wallpaper

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -101,6 +101,6 @@ lib.optionalAttrs (!linux) {
 
   programs.setWallpaper = {
     enable = true;
-    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/misc/wtc.jpg;
+    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/nsfw/image.png;
   };
 }
