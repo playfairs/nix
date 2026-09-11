12b39e72a158087533d1aa8b19e2c9df4e4caefb
playfairs <root@playfairs.cc>
2026-09-11 00:41:24 -0500

    ok

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -101,6 +101,6 @@ lib.optionalAttrs (!linux) {
 
   programs.setWallpaper = {
     enable = true;
-    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/misc/pools.png;
+    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/misc/twin.jpg;
   };
 }
