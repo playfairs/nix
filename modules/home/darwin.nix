620391269fff527587e62a59d22c833c126d4a59
playfairs <root@playfairs.cc>
2026-09-14 02:31:28 -0500

    add a dynamic wallpaper

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -101,6 +101,6 @@ lib.optionalAttrs (!linux) {
 
   programs.setWallpaper = {
     enable = true;
-    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/misc/twin.jpg;
+    wallpaperPath = flakeConfig.user.wallpaper or ../../wallpapers/apple/Dynamic.heic;
   };
 }
